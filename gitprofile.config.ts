fe0be44b39f96edde2545a2ce42cb8349d0cd66d
Amanda F <44936062+nekkuzuria@users.noreply.github.com>
2024-10-31 22:26:17 +0700

    fix typo

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -130,7 +130,7 @@ const CONFIG = {
   blog: {
     source: '', // medium | dev
     username: '', // to hide blog section, keep it empty
-    limit: , // How many articles to display. Max is 10.
+    limit: '', // How many articles to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
